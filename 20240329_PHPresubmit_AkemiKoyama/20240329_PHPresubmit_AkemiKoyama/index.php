45a3fe4ed4293c1c2b1661ad1774c593b5ff7344
ALK <165754555+alkoyama@users.noreply.github.com>
2024-04-03 13:05:43 +0900

    Update index.php
    Deleted <script> as now irrelavant

--- 20240329_PHPresubmit_AkemiKoyama/20240329_PHPresubmit_AkemiKoyama/index.php
+++ 20240329_PHPresubmit_AkemiKoyama/20240329_PHPresubmit_AkemiKoyama/index.php
@@ -39,18 +39,6 @@
         </form>
         </center>
 
-        <script>
-           function validateForm() {
-            var itemA = document.getElementById("itemA").value;
-            var itemB = document.getElementById("itemB").value;
-
-            if (parseInt(itemA) <= 0 && parseInt(itemB) <= 0) {
-                alert("おともだちをひとり以上選ばないと注文を送信できません。");
-                return false;
-            }
-
-            return true;
-            }
-       </script>
+    <script src="blankjs.js" defer></script>
 </body>
 </html>
